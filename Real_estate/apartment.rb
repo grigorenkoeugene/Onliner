8b24884cfcd35b5869dfb64bad41513cbbceccd1
Joka <7327917@gmail.com>
2022-11-21 12:54:19 +0300

    apartment add for realEstate

---
+++ Real_estate/apartment.rb
@@ -0,0 +1,12 @@
+
+module Onliner
+  module RealEstate
+    class Apartment 
+      def initialize(rooms_count, square_meters, street)
+        @rooms_count = rooms_count
+        @square_meters = square_meters
+        @street = street
+      end
+    end
+  end
+end
