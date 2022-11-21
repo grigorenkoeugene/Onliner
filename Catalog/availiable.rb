738532486b3ac058cc9df6ab616f0f3d92278162
Joka <7327917@gmail.com>
2022-11-21 12:51:42 +0300

    availiablle add for catalog

---
+++ Catalog/availiable.rb
@@ -0,0 +1,28 @@
+require_relative "./product"
+
+module Onliner
+  module Catalog
+    class Availiable 
+      def initialize
+          prodact = Product.new
+      end
+
+      def add_product_to_catalog(prodact)
+          
+      end
+
+      def search_by_name 
+          # method 
+      end
+
+      def search_by_price 
+          # method 
+      end
+
+      def search_by_manufacturer 
+          # method 
+      end
+    end
+  end
+end
+
