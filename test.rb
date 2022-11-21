4b76a2c3a3ce4b023c100781e8ff49722fb38b77
Joka <7327917@gmail.com>
2022-11-21 14:58:33 +0300

    add test file for catalog

---
+++ test.rb
@@ -0,0 +1,17 @@
+require_relative "./catalog/product"
+require_relative "./catalog/availiable"
+
+module Onliner
+  module Catalog
+    products_catalog = Availiable.new
+
+    product_catalog1 = Product.new("Govno", 25, "Xiaomi")
+    product_catalog2 = Product.new("Ne govno", 27, "Apple")
+
+    products_catalog.add_product_to_catalog(product_catalog1)
+    products_catalog.add_product_to_catalog(product_catalog2)
+
+    products_catalog.search_by_manufacturer("Apple")
+    products_catalog.search_by_name("Govno")
+  end
+end
